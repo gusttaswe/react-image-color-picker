67af11b0b47d78b056725f23ebb603bda4b61ef3
Gustavo Mata <dev.gustta.h@gmail.com>
2023-06-20 15:32:59 -0300

    chore: remove dist folder

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-interface Props {
-    text: string;
-}
-export declare const ExampleComponent: ({ text }: Props) => React.JSX.Element;
-export {};
--- dist/stories/Header.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Header } from './Header';
-declare const meta: Meta<typeof Header>;
-export default meta;
-declare type Story = StoryObj<typeof Header>;
-export declare const LoggedIn: Story;
-export declare const LoggedOut: Story;
