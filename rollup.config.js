399e0f57df6471a56b190a3e62ce6967b5221b38
Gustavo Mata <dev.gustta.h@gmail.com>
2023-06-26 11:21:39 -0300

    build(rollup): fix styled-componentes dependency error

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,35 @@
 import typescript from 'rollup-plugin-typescript2'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import autoprefixer from 'autoprefixer'
-
-const pkg = require('./package.json')
+import external from 'rollup-plugin-peer-deps-external'
+import terser from '@rollup/plugin-terser'
+import babel from '@rollup/plugin-babel'
+import pkg from './package.json'
 
 export default {
-  input: 'src/index.tsx',
-  external: ['styled-components'],
+  input: pkg.source,
   output: [
     {
       file: pkg.main,
       format: 'cjs',
-      sourcemap: true
+      sourcemap: true,
+      exports: 'named'
     },
     {
       file: pkg.module,
       format: 'esm',
-      sourcemap: true
+      sourcemap: true,
+      exports: 'named'
     }
   ],
   plugins: [
-    peerDepsExternal(),
+    external(),
+    terser(),
+    babel({
+      exclude: ['node_modules/**'],
+      babelHelpers: 'bundled'
+    }),
     typescript({
       tsconfig: './tsconfig.json'
-    }),
-    postcss({
-      extract: true, // Extract CSS to a separate file
-      minimize: true, // Minify the CSS
-      modules: true,
-      plugins: [autoprefixer()] // Add any additional PostCSS plugins
     })
-  ]
+  ],
+  external: ['styled-components']
 }
